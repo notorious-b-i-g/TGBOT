fb2a71aee7ee99bf55adbaf47a218091ed5494ee
2pacAlive <sakanyan1488@gmail.com>
2026-06-06 18:48:00 +0300

    Move secrets to .env, add .gitignore and .env.example

--- config.py
+++ config.py
@@ -1,19 +1,28 @@
-API_TOKEN = '***REVOKED-TELEGRAM-TOKEN***'
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_TOKEN = os.getenv("API_TOKEN")
 
 DB_CONFIG = {
-    'host': '127.0.0.1',  # Локальный хост, так как мы будем использовать туннель
-    'port': 3306,
-    'user': 'h540890_viniki',
-    'password': '***REVOKED-DB-PASSWORD***',
-    'db': 'h540890_viniki'
+    "host": os.getenv("DB_HOST", "127.0.0.1"),  # Локальный хост, так как используем туннель
+    "port": int(os.getenv("DB_PORT", "3306")),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "db": os.getenv("DB_NAME"),
 }
 
 SSH_CONFIG = {
-    'ssh_host': 'ssh.hosting.masterhost.ru',
-    'ssh_port': 22,
-    'ssh_user': 'h540890',
-    'ssh_password': '***REVOKED-SSH-PASSWORD***',
-    'remote_bind_address': ('h540890_viniki.mysql.masterhost.ru', 3306)
+    "ssh_host": os.getenv("SSH_HOST"),
+    "ssh_port": int(os.getenv("SSH_PORT", "22")),
+    "ssh_user": os.getenv("SSH_USER"),
+    "ssh_password": os.getenv("SSH_PASSWORD"),
+    "remote_bind_address": (
+        os.getenv("REMOTE_DB_HOST"),
+        int(os.getenv("REMOTE_DB_PORT", "3306")),
+    ),
 }
 
-admins = ['chinanumb2', 'burzhuykaa','fullmetalldickk']
+admins = ["chinanumb2", "burzhuykaa", "fullmetalldickk"]
